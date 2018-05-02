893186f940707d2e4f34ff4238585530841e4a2c
Senthil <Senthil@192.168.1.208>
2018-05-02 12:46:36 +0530

    Added steps

--- FreeCRMBDDFrameWork/src/test/java/TestClass.java
+++ FreeCRMBDDFrameWork/src/test/java/TestClass.java
@@ -3,6 +3,7 @@ public class TestClass {
 	public static void main(String args[]){
 		System.out.println("Hi Senthil");
 		System.out.println("Welcome");
+		System.out.println("Good Morning");
 	}
 
 }
